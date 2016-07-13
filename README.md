3dc571c454b559e8182371932754a98e44687905
valmassoi <rvalmassoi@gmail.com>
2016-07-13 12:52:20 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# dash-btn
+Amazon Dash Button Hack - prints and speaks Bitstamp bitcoin price
+
+## Setup
+Follow Amazon setup instruction, except the last step (selecting product)
+`$ git clone git@github.com:valmassoi/dash-btn.git`
+`$ cd dash-btn`
+`$ sudo python dash_sniff.py`
+Push button
+
+## TODO
+[ ] Fix: command runs twice per button push
---
+++ dash_sniff.py
@@ -0,0 +1,24 @@
+# Thanks to Ted Benson: https://medium.com/@edwardbenson/how-i-hacked-amazon-s-5-wifi-button-to-track-baby-data-794214b0bdd8#.g4yme08zh
+from scapy.all import *
+import requests
+import json
+import time
+import os
+
+def bitcoin():
+    print time.time()
+    r = requests.get("https://www.bitstamp.net/api/ticker_hour/")
+    data = json.loads(r.content)
+    os.system("say bitcoin price is" +data['last'])
+    print data['last']
+
+def arp_display(pkt):
+  if pkt[ARP].op == 1: #who-has (request)
+    #if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+      if pkt[ARP].hwsrc == 'f0:27:2d:f0:0a:60': # Clorox
+        print "Clorox Pushed: f0:27:2d:f0:0a:60"
+        bitcoin()
+      else:
+        print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+
+print sniff(prn=arp_display, filter="arp", store=0) #count=100)
