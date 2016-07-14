48c4f645aee8d5d3ba420e8bec15bc474204cd81
valmassoi <rvalmassoi@gmail.com>
2016-07-14 16:36:56 -0700

    added 2nd device

--- dash_sniff.py
+++ dash_sniff.py
@@ -4,21 +4,30 @@ import requests
 import json
 import time
 import os
+from devices import *
 
-def bitcoin():
+def sayBitcoin():
     print time.time()
     r = requests.get("https://www.bitstamp.net/api/ticker_hour/")
     data = json.loads(r.content)
-    os.system("say bitcoin price is" +data['last'][0:3])
-    print data['last']
+    lastPrice = data['last']
+    os.system("say bitcoin price is" +lastPrice[0:3])
+    print lastPrice
+
+def remoteLock():
+    os.system("gnome-screensaver-command -a")
 
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
-    #if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-      if pkt[ARP].hwsrc == 'f0:27:2d:f0:0a:60': # Clorox MAC ADDRESS
-        print "Clorox Pushed: f0:27:2d:f0:0a:60"
-        bitcoin()
-      else:
-        print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+    print pkt[ARP].psrc
+    if pkt[ARP].psrc != '192.168.1.1': # ARP Probe
+        if pkt[ARP].hwsrc == MAC_ADDRESS_CLOROX:
+            print "Clorox Pushed: " + MAC_ADDRESS_CLOROX
+            sayBitcoin()
+        elif pkt[ARP].hwsrc == MAC_ADDRESS_CHARMIN:
+            print "Charmin Pushed: " + MAC_ADDRESS_CHARMIN
+            remoteLock()
+        else:
+            print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
 
 print sniff(prn=arp_display, filter="arp", store=0) #count=100)
